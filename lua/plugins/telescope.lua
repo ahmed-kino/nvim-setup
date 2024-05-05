daf1383ecc31d9d8705b75bd23d543c4c8c2c204
Ahmed Kino <ehmedaff@gmail.com>
2024-05-05 01:23:49 +0100

    Basic LSP setup for lua + JS

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+  {
   'nvim-telescope/telescope.nvim', tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
@@ -7,4 +8,20 @@ return {
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>gst", builtin.git_status, {})
   end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
