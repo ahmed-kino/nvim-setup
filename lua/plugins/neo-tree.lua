68369e62fae3b125432912d62ba156026a1523fd
Ahmed Kino <ehmedaff@gmail.com>
2024-06-01 21:11:12 +0100

    Hilight the current buffer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,19 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-b>", ":Neotree toggle left<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-b>", ":Neotree toggle left<CR>")
+  end,
 }
